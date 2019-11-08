90db59855ec70d9710763c0a2baf810cbb88cf1b
DavidWaichari <dwaichari@gmail.com>
2019-11-08 20:08:12 +0300

    use bootstrap cdn for css

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ class CreateUsersTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->string('phone');
-            $table->string('alternativephone');
+            $table->string('alternativephone')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->boolean('is_admin')->default(false);
