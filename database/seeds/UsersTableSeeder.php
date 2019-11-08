7d82733f18b9d14aeaf2c1c593350086c8f2da0e
DavidWaichari <dwaichari@gmail.com>
2019-11-08 16:06:31 +0300

    Make seeders for products and one User

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = new User;
+        $user->name = "Admin";
+        $user->email = "admin@devtest.com";
+        $user->phone = "0708473015";
+        $user->alternativephone = "0708473015";
+        $user->password = bcrypt('secret');
+        $user->is_admin = true;
+        $user->save();
+    }
+}
