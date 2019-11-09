f82d5ac9c7a19b9d5e87ea2e190ec9226ac765d4
DavidWaichari <dwaichari@gmail.com>
2019-11-09 11:06:12 +0300

    rectify testCreateOrder method on OrderTest

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -1,7 +1,7 @@
 <?php
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
-use App\User;
+use App\Product;
 use Faker\Generator as Faker;
 use Illuminate\Support\Str;
 
@@ -16,13 +16,10 @@ use Illuminate\Support\Str;
 |
 */
 
-$factory->define(User::class, function (Faker $faker) {
+$factory->define(Product::class, function (Faker $faker) {
     return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        'phone' => $faker->unique()->e164PhoneNumber,
-        'email_verified_at' => now(),
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-        'remember_token' => Str::random(10),
+        'product_id' => $faker->unique()->uuid,
+        'quantity' => numberBetween($min = 10, $max = 100) ,
+        'address' => $faker->unique()->address
     ];
 });
